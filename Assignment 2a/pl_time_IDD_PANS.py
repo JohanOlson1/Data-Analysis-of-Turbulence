c6de683c0284f9c3264ef6bd3d4ea92a1ae2db59
JohanOlson <62870685+JohanOlson@users.noreply.github.com>
2021-05-04 15:11:37 +0200

    4 may

--- Assignment 2a/pl_time_IDD_PANS.py
+++ Assignment 2a/pl_time_IDD_PANS.py
@@ -1,4 +1,6 @@
 import numpy as np
+import pandas
+import tkinter as tk
 import matplotlib.pyplot as plt
 from scipy.signal import welch, hann
 from IPython import display
@@ -35,26 +37,248 @@ t_tot=dt*len(u1)
 
 t = np.linspace(0,t_tot,len(u1))
 
-# %%%%%%%%%%%%%%%% plotting section %%%%%%%%%%%%%%%%%%%%%%%%%%
-# plot u
-fig1,ax1 = plt.subplots()
-plt.subplots_adjust(left=0.20,bottom=0.20)
+# ---- Plots 
+def node_1_2_3_velocity_v1():
+    fig1,ax1 = plt.subplots()
+    plt.subplots_adjust(left=0.20,bottom=0.20)
+    plt.plot(t,u1,'b-', label='$Node\:\: 1$')
+    plt.plot(t,u2,'r-',  label='$Node\:\: 2$')
+    plt.plot(t,u3,'k-',  label='$Node\:\: 3$')
+    plt.title('$v_1$ each node')
+    plt.xlabel("$t$")
+    plt.ylabel("$v_1$")
+    plt.legend()
+    
+    # zoom
+    fig1,ax1 = plt.subplots()
+    plt.subplots_adjust(left=0.20,bottom=0.20)
+    plt.plot(t,u1,'b-', label='$Node\:\: 1$')
+    plt.plot(t,u2,'r-',  label='$Node\:\: 2$')
+    plt.plot(t,u3,'k-',  label='$Node\:\: 3$')
+    plt.title('$v_1$ each node, zoom')
+    plt.xlabel("$t$")
+    plt.ylabel("$v_1$")
+    plt.legend()
+    
+    plt.axis([6, 7, 7, 30])
+    
+def node_1_2_3_velocity_v3():
+    fig1,ax1 = plt.subplots()
+    plt.subplots_adjust(left=0.20,bottom=0.20)
+    plt.plot(t,w1,'b-', label='$Node\:\: 1$')
+    plt.plot(t,w2,'r-',  label='$Node\:\: 2$')
+    plt.plot(t,w3,'k-',  label='$Node\:\: 3$')
+    plt.title('$v_3$ each node')
+    plt.xlabel("$t$")
+    plt.ylabel("$v_3$")
+    plt.legend()
+    
+    # zoom
+    fig1,ax1 = plt.subplots()
+    plt.subplots_adjust(left=0.20,bottom=0.20)
+    plt.plot(t,w1,'b-', label='$Node\:\: 1$')
+    plt.plot(t,w2,'r-',  label='$Node\:\: 2$')
+    plt.plot(t,w3,'k-',  label='$Node\:\: 3$')
+    plt.title('$v_3$ each node, zoom')
+    plt.xlabel("$t$")
+    plt.ylabel("$v_3$")
+    plt.legend()
+    
+    plt.axis([6, 7, -3, 3])
+    
+def node_4_5_velocity_v1():
+    fig1,ax1 = plt.subplots()
+    plt.subplots_adjust(left=0.20,bottom=0.20)
+    plt.plot(t,u4,'m-', label='$Node\:\: 4$')
+    plt.plot(t,u5,'g-',  label='$Node\:\: 5$')
+    plt.title('$v_1$ each node')
+    plt.xlabel("$t$")
+    plt.ylabel("$v_1$")
+    plt.legend()
+    
+    # zoom
+    fig1,ax1 = plt.subplots()
+    plt.subplots_adjust(left=0.20,bottom=0.20)
+    plt.plot(t,u4,'m-', label='$Node\:\: 4$')
+    plt.plot(t,u5,'g-',  label='$Node\:\: 5$')
+    plt.title('$v_1$ each node, zoom')
+    plt.xlabel("$t$")
+    plt.ylabel("$v_1$")
+    plt.legend()
+    
+    plt.axis([6, 7, 7, 30])
+    
+def node_4_5_velocity_v3():
+    fig1,ax1 = plt.subplots()
+    plt.subplots_adjust(left=0.20,bottom=0.20)
+    plt.plot(t,w4,'m-', label='$Node\:\: 4$')
+    plt.plot(t,w5,'g-',  label='$Node\:\: 5$')
+    plt.title('$v_3$ each node')
+    plt.xlabel("$t$")
+    plt.ylabel("$v_3$")
+    plt.legend()
+    
+    # zoom
+    fig1,ax1 = plt.subplots()
+    plt.subplots_adjust(left=0.20,bottom=0.20)
+    plt.plot(t,w4,'m-', label='$Node\:\: 4$')
+    plt.plot(t,w5,'g-',  label='$Node\:\: 5$')
+    plt.title('$v_3$ each node, zoom')
+    plt.xlabel("$t$")
+    plt.ylabel("$v_3$")
+    plt.legend()
+    
+    plt.axis([6, 7, -3, 3])
+        
+# Autocorrelation
 
-plt.plot(t,u1,'b--')
-plt.plot(t,u4,'r-')
-plt.xlabel("$t$")
-plt.ylabel("$u$")
+## node 1 v_1
+u1_fluct=u1-np.mean(u1)
+node1v1=np.correlate(u1_fluct,u1_fluct,'full')
+# find max
+nmax_node1v1=np.argmax(node1v1)
+# and its value
+node1v1_max=np.max(node1v1)   
+# two_max is symmwetric. Pick the right half and normalize
+node1v1_sym_norm=node1v1[nmax_node1v1:]/node1v1_max
 
-plt.savefig('utime_python.eps',bbox_inches='tight')
+int_T_1 = np.trapz(node1v1_sym_norm)*dt
 
-####################### # zoom
-fig1,ax1 = plt.subplots()
-plt.subplots_adjust(left=0.20,bottom=0.20)
-plt.plot(t,u1,'b--')
-plt.plot(t,u4,'r-')
-plt.xlabel("$t$")
-plt.ylabel("$u$")
+## node 2 v_1
+u2_fluct=u2-np.mean(u2)
+node2v1=np.correlate(u2_fluct, u2_fluct,'full')
+# find max
+nmax_node2v1=np.argmax(node2v1)
+# and its value
+node2v1_max=np.max(node2v1)   
+# two_max is symmwetric. Pick the right half and normalize
+node2v1_sym_norm=node2v1[nmax_node2v1:]/node2v1_max
 
-plt.axis([6, 7, 7, 22])
+int_T_2 = np.trapz(node2v1_sym_norm)*dt
 
-plt.savefig('utime_zoom_python.eps',bbox_inches='tight')
+#3 node 3 v_1
+u3_fluct=u3-np.mean(u3)
+node3v1=np.correlate(u3_fluct, u3_fluct,'full')
+# find max
+nmax_node3v1=np.argmax(node3v1)
+# and its value
+node3v1_max=np.max(node3v1)   
+# two_max is symmwetric. Pick the right half and normalize
+node3v1_sym_norm=node3v1[nmax_node3v1:]/node3v1_max
+
+int_T_3 = np.trapz(node3v1_sym_norm)*dt
+
+## node 4 v_1
+u4_fluct=u4-np.mean(u4)
+node4v1=np.correlate(u4_fluct, u4_fluct,'full')
+# find max
+nmax_node4v1=np.argmax(node4v1)
+# and its value
+node4v1_max=np.max(node4v1)   
+# two_max is symmwetric. Pick the right half and normalize
+node4v1_sym_norm=node4v1[nmax_node4v1:]/node4v1_max
+
+int_T_4 = np.trapz(node4v1_sym_norm)*dt
+
+# node 5 v_1
+u5_fluct=u5-np.mean(u5)
+node5v1=np.correlate(u5_fluct, u5_fluct,'full')
+# find max
+nmax_node5v1=np.argmax(node5v1)
+# and its value
+node5v1_max=np.max(node5v1)   
+# two_max is symmwetric. Pick the right half and normalize
+node5v1_sym_norm=node5v1[nmax_node5v1:]/node5v1_max
+
+int_T_5 = np.trapz(node5v1_sym_norm)*dt
+
+
+
+imax=500;
+def autocorr_node_1():
+    plt.figure("autocorr_node_1")
+    plt.title('Autocorrelation Node 1: $v_1$')
+    plt.plot(t[0:imax],node1v1_sym_norm[0:imax],'b-')
+    plt.xlabel('t')
+    plt.ylabel('$B_{v_1 v_1}$')
+    
+def autocorr_node_2():
+    plt.figure("autocorr_node_2")
+    plt.title('Autocorrelation Node 2: $v_1$')
+    plt.plot(t[0:imax],node2v1_sym_norm[0:imax],'b-')
+    plt.xlabel('t')
+    plt.ylabel('$B_{v_1 v_1}$')
+
+def autocorr_node_3():
+    plt.figure("autocorr_node_3")
+    plt.title('Autocorrelation Node 2: $v_1$')
+    plt.plot(t[0:imax],node3v1_sym_norm[0:imax],'b-')
+    plt.xlabel('t')
+    plt.ylabel('$B_{v_1 v_1}$')
+
+def autocorr_node_4():
+    plt.figure("autocorr_node_4")
+    plt.title('Autocorrelation Node 2: $v_1$')
+    plt.plot(t[0:imax],node4v1_sym_norm[0:imax],'b-')
+    plt.xlabel('t')
+    plt.ylabel('$B_{v_1 v_1}$')
+
+def autocorr_node_5():
+    plt.figure("autocorr_node_5")
+    plt.title('Autocorrelation Node 2: $v_1$')
+    plt.plot(t[0:imax],node5v1_sym_norm[0:imax],'b-')
+    plt.xlabel('t')
+    plt.ylabel('$B_{v_1 v_1}$')
+    
+def integral_timescales_v_1():
+    node = ["Node 1:", "Node 2:", "Node 3:", "Node 4:", "Node 5:"]
+    timescales = [int_T_1, int_T_2, int_T_3, int_T_4, int_T_5]
+    
+    table = pandas.DataFrame(timescales, node)
+    
+    print(table)
+    
+#---- GUI Append
+
+def close_fig():
+    plt.close()
+    
+root = tk.Tk()
+close_button = tk.Button(root, text='Close plot', command = close_fig)
+close_button.grid(row=0, column=0)
+
+# Overview Plots
+label_overview = tk.Label(text="U1", background="grey")
+label_overview.grid(row=0, column=1, sticky='nesw')
+
+button_U1_123_v1 = tk.Button(root, text= 'node 1, 2, 3, v_1', command = node_1_2_3_velocity_v1)
+button_U1_123_v1.grid(row=1, column=1, sticky='nesw')
+
+button_U1_123_v3 = tk.Button(root, text= 'node 1, 2, 3, v_3', command = node_1_2_3_velocity_v3)
+button_U1_123_v3.grid(row=2, column=1, sticky='nesw')
+
+button_U1_45_v1 = tk.Button(root, text= 'node 4, 5, v_1', command = node_4_5_velocity_v1)
+button_U1_45_v1.grid(row=3, column=1, sticky='nesw')
+
+button_U1_45_v3 = tk.Button(root, text= 'node 4, 5 v_3', command = node_4_5_velocity_v3)
+button_U1_45_v3.grid(row=4, column=1, sticky='nesw')
+
+button_U1_autocorr_N1_v_1 = tk.Button(root, text= 'Autocorr Node 1, v_1', command = autocorr_node_1)
+button_U1_autocorr_N1_v_1.grid(row=5, column=1, sticky='nesw')
+
+button_U1_autocorr_N2_v_1 = tk.Button(root, text= 'Autocorr Node 2, v_1', command = autocorr_node_2)
+button_U1_autocorr_N2_v_1.grid(row=6, column=1, sticky='nesw')
+
+button_U1_autocorr_N3_v_1 = tk.Button(root, text= 'Autocorr Node 3, v_1', command = autocorr_node_3)
+button_U1_autocorr_N3_v_1.grid(row=7, column=1, sticky='nesw')
+
+button_U1_autocorr_N4_v_1 = tk.Button(root, text= 'Autocorr Node 4, v_1', command = autocorr_node_4)
+button_U1_autocorr_N4_v_1.grid(row=8, column=1, sticky='nesw')
+
+button_U1_autocorr_N5_v_1 = tk.Button(root, text= 'Autocorr Node 5, v_1', command = autocorr_node_5)
+button_U1_autocorr_N5_v_1.grid(row=9, column=1, sticky='nesw')
+
+button_U1_timescale_table = tk.Button(root, text= 'Integral Timescales', command = integral_timescales_v_1)
+button_U1_timescale_table.grid(row=10, column=1, sticky='nesw')
+
+root.mainloop()
--- Assignment 2a/pl_uvw_IDD_PANS.py
+++ Assignment 2a/pl_uvw_IDD_PANS.py
@@ -1,5 +1,6 @@
 import scipy.io as sio
 import numpy as np
+import tkinter as tk
 import sys
 import matplotlib.pyplot as plt
 plt.rcParams.update({'font.size': 22})
@@ -17,7 +18,7 @@ nk=34
 
 viscos=1./5200.
 
-#%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%  read v_1 & transform v_1 to a 3D array (file 1)
+#----  read v_1 & transform v_1 to a 3D array (file 1)
 uvw = sio.loadmat('u1_IDD_PANS.mat')
 ttu=uvw['u1_IDD_PANS']
 u3d1= np.reshape(ttu,(nk,nj,ni))
@@ -40,7 +41,7 @@ tt=uvw['eps1_IDD_PANS']
 eps3d1= np.reshape(tt,(nk,nj,ni))
 
 
-#%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%  read v_2 & transform v_2 to a 3D array (file 2)
+#----  read v_2 & transform v_2 to a 3D array (file 2)
 uvw = sio.loadmat('u2_IDD_PANS.mat')
 ttu=uvw['u2_IDD_PANS']
 u3d2= np.reshape(ttu,(nk,nj,ni))
@@ -126,15 +127,226 @@ for i in range (0,40):
    i1 = (np.abs(10.**xx-yplus_DNS)).argmin()
    jDNS[i]=int(i1)
    xx=xx+0.2
+   
+# ---- Plot
 
-############################### U
-fig1,ax1 = plt.subplots()
-plt.subplots_adjust(left=0.20,bottom=0.20)
+## U2
+def plot_mean_velocity_profile():
+    fig1,ax1 = plt.subplots()
+    plt.subplots_adjust(left=0.20,bottom=0.20)
+    
+    plt.semilogx(yplus,umean/ustar,'b-')
+    plt.semilogx(yplus_DNS[jDNS],u_DNS[jDNS],'bo')
+    plt.axis([1, 8000, 0, 31])
+    plt.ylabel("$U^+$")
+    plt.xlabel("$y^+$")
+    
+uvmean1= np.mean((u3d-umean[None,:,None])*(v3d-vmean[None,:,None]), axis=(0,2))
+uumean = np.mean((u3d-umean[None,:,None])*(u3d-umean[None,:,None]), axis=(0,2))
+vvmean = np.mean((v3d-vmean[None,:,None])*(v3d-vmean[None,:,None]), axis=(0,2))
+wwmean = np.mean((w3d-wmean[None,:,None])*(w3d-wmean[None,:,None]), axis=(0,2))
 
-plt.semilogx(yplus,umean/ustar,'b-')
-plt.semilogx(yplus_DNS[jDNS],u_DNS[jDNS],'bo')
-plt.axis([1, 8000, 0, 31])
-plt.ylabel("$U^+$")
-plt.xlabel("$y^+$")
+te_resolved = 0.5*(uumean + vvmean + wwmean)
 
-plt.savefig('u_log_zonal_python.eps',bbox_inches='tight')
+## U3
+
+def uv_stress_resolved():
+    plt.figure("uv_Stress_resolved")
+    plt.plot(yplus, uvmean1)
+    plt.title('Resolved uv Stress')
+    plt.ylabel("$U^+$")
+    plt.xlabel("$y^+$")
+
+## U4
+def te_plot():
+    plt.figure("Turbulent Kinetic Energy")
+    plt.plot(yplus, temean, label='$k_{modelled}$')
+    plt.plot(yplus, te_resolved, label='$k_{resolved}$')
+    plt.title('Turbulent Kinetic Energy')
+    plt.ylabel("$U^+$")
+    plt.xlabel("$y^+$")
+    plt.legend()
+    
+## U5
+Cmu = 0.09
+
+nu_t = Cmu*np.divide(np.multiply(temean, temean), epsmean)
+    
+dudx, dudy, dudz=np.gradient(u3d,dx,y,dz)
+
+dvdx, dvdy, dvdz=np.gradient(v3d,dx,y,dz)
+
+dwdx, dwdy, dwdz=np.gradient(w3d,dx,y,dz)
+
+# Time average
+
+dudymean = np.mean(dudy, axis=(0,2))
+dvdxmean = np.mean(dvdx, axis=(0,2))
+
+tau12 = - np.multiply(nu_t, dudymean + dvdxmean)
+
+def turbulent_shear():
+    plt.figure("Turbulent Shear")
+    plt.plot(yplus, tau12, label='$\\tau_{12}$')
+    plt.plot(yplus, uvmean1, label='$\\tau_{resolved}$')
+    plt.title('Turbulent Kinetic Energy')
+    plt.ylabel("Shear")
+    plt.xlabel("$y^+$")
+    plt.legend()
+    
+## U6
+
+# some calc.
+
+def boundary_interface():
+    plt.figure("boundary interface")
+
+## U7
+
+kappa = 0.41
+
+zeta = 1
+
+# 1D
+
+dumeandy = np.gradient(umean, y)
+dumeandy2 = np.gradient(dumeandy, y)
+
+L_v_K_1D = kappa * np.abs(np.divide(dumeandy, dumeandy2))
+
+S_1D = 2*np.multiply(dumeandy2, dumeandy2)
+
+L_1D = (Cmu**(3/4))*np.multiply(np.power(temean, 3/2), epsmean)
+
+T_1_1D = zeta*kappa*np.divide(np.multiply(np.multiply(S_1D,S_1D), L_1D), L_v_K_1D)
+
+# 3D
+
+# u
+dudx2, empty1, empty2 = np.gradient(dudx,dx,y,dz)
+
+empty1, dudy2, empty2 = np.gradient(dudy,dx,y,dz)
+
+empty1, empty2, dudz2 = np.gradient(dudz,dx,y,dz)
+# v
+dvdx2, empty1, empty2 = np.gradient(dvdx,dx,y,dz)
+
+empty1, dvdy2, empty2 = np.gradient(dvdy,dx,y,dz)
+
+empty1, empty2, dvdz2 = np.gradient(dvdz,dx,y,dz)
+# w
+dwdx2, empty1, empty2 = np.gradient(dwdx,dx,y,dz)
+
+empty1, dwdy2, empty2 = np.gradient(dwdy,dx,y,dz)
+
+empty1, empty2, dwdz2 = np.gradient(dwdz,dx,y,dz)
+
+# Partials
+Lambda1 = dudx2 + dudy2 + dudz2
+Lambda2 = dvdx2 + dvdy2 + dvdz2
+Lambda3 = dwdx2 + dwdy2 + dwdz2
+
+Gamma1 = dudx2 + dudy2 + dudz2
+Gamma2 = dvdx2 + dvdy2 + dvdz2
+Gamma3 = dwdx2 + dwdy2 + dwdz2
+
+
+S = np.zeros((ni,nj,nk))
+Ubiss_1 = np.zeros((ni,nj,nk))
+
+for i in range(ni):
+    for j in range(nj):
+        for k in range(nk):
+            S[i,j,k] += dudx[i,j,k]*dudx[i,j,k]
+            S[i,j,k] += dudy[i,j,k]*dudy[i,j,k]
+            S[i,j,k] += dudz[i,j,k]*dudz[i,j,k]
+            S[i,j,k] += dvdx[i,j,k]*dvdx[i,j,k]
+            S[i,j,k] += dvdy[i,j,k]*dvdy[i,j,k]
+            S[i,j,k] += dvdz[i,j,k]*dvdz[i,j,k]
+            S[i,j,k] += dwdx[i,j,k]*dwdx[i,j,k]
+            S[i,j,k] += dwdy[i,j,k]*dwdy[i,j,k]
+            S[i,j,k] += dwdz[i,j,k]*dwdz[i,j,k]
+            
+            Ubiss_1[i,j,k] += Gamma1[i,j,k]*Lambda1[i,j,k]
+            Ubiss_1[i,j,k] += Gamma2[i,j,k]*Lambda2[i,j,k]
+            Ubiss_1[i,j,k] += Gamma3[i,j,k]*Lambda3[i,j,k]
+            
+for i in range(ni):
+    for j in range(nj):
+        for k in range(nk):
+            S[i,j,k] = np.sqrt(2*S[i,j,k])            
+            Ubiss_1[i,j,k] = np.sqrt(Ubiss_1[i,j,k]) 
+
+L_v_K_3D = kappa*np.abs(np.divide(S, Ubiss_1))
+
+L = (Cmu**(3/4))*np.multiply(np.power(te3d, 3/2), eps3d)
+
+T_1_3D_1 = zeta*kappa*np.divide(np.multiply(np.multiply(S,S), L), L_v_K_3D) 
+
+# Plot
+L_v_K_1D_mean_after = np.mean(L_v_K_3D , axis=(0,2))
+
+def length_scale_compare():
+    plt.figure("Length_scale_compare")
+    plt.plot(yplus, L_v_K_1D , label='1D before')
+    plt.plot(yplus, L_v_K_1D_mean_after , label='1D After')
+    plt.title('Length_scale_compare')
+    plt.ylabel("Length Scale")
+    plt.xlabel("$y^+$")
+    plt.legend()
+    
+# ---- GUI Append
+
+def close_fig():
+    plt.close()
+    
+root = tk.Tk()
+close_button = tk.Button(root, text='Close plot', command = close_fig)
+close_button.grid(row=0, column=0)
+
+
+## Overview Plots
+# U2
+label_overview = tk.Label(text="U2", background="grey")
+label_overview.grid(row=0, column=1, sticky='nesw')
+
+button_mean_velocity_profile = tk.Button(root, text= 'Mean velocity Profile v_1', command = plot_mean_velocity_profile)
+button_mean_velocity_profile.grid(row=1, column=1, sticky='nesw')
+
+# U3
+label_overview = tk.Label(text="U3", background="grey")
+label_overview.grid(row=0, column=2, sticky='nesw')
+
+button_uv = tk.Button(root, text= 'uv Resolved', command = uv_stress_resolved)
+button_uv.grid(row=1, column=2, sticky='nesw')
+
+# U4
+label_overview = tk.Label(text="U4", background="grey")
+label_overview.grid(row=0, column=3, sticky='nesw')
+
+button_te = tk.Button(root, text= 'Resolved tubulent Kinetic Energy', command = te_plot)
+button_te.grid(row=1, column=3, sticky='nesw')
+
+# U5
+label_overview = tk.Label(text="U5", background="grey")
+label_overview.grid(row=0, column=4, sticky='nesw')
+
+button_turbulent_shear = tk.Button(root, text= 'Turbulent Shear', command = turbulent_shear)
+button_turbulent_shear.grid(row=1, column=4, sticky='nesw')
+
+# U6
+label_overview = tk.Label(text="U6", background="grey")
+label_overview.grid(row=0, column=5, sticky='nesw')
+
+button_boundary_interface = tk.Button(root, text= 'Boundary Interface DDES', command = boundary_interface)
+button_boundary_interface.grid(row=1, column=5, sticky='nesw')
+
+# U7
+label_overview = tk.Label(text="U7", background="grey")
+label_overview.grid(row=0, column=6, sticky='nesw')
+
+button_length_scale_compare = tk.Button(root, text= 'Length_scale_compare', command = length_scale_compare)
+button_length_scale_compare.grid(row=1, column=6, sticky='nesw')
+
+root.mainloop()
+
