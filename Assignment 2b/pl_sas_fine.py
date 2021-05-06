31096bad9ac9c5dfe1914e329ff6deb44818fd16
JohanOlson <62870685+JohanOlson@users.noreply.github.com>
2021-05-06 14:20:58 +0200

    2b start

---
+++ Assignment 2b/pl_sas_fine.py
@@ -0,0 +1,251 @@
+import scipy.io as sio
+import numpy as np
+import matplotlib.pyplot as plt
+from dphidx_dy import dphidx_dy
+plt.rcParams.update({'font.size': 22})
+plt.interactive(True)
+
+
+re =9.36e+5
+viscos =1/re
+
+nk=66
+nk=34
+dz=0.2/(nk-2)
+
+xy_hump_fine = np.loadtxt("xy_hump_fine.dat")
+x1=xy_hump_fine[:,0]
+y1=xy_hump_fine[:,1]
+
+
+nim1=int(x1[0])
+njm1=int(y1[0])
+
+ni=nim1+1
+nj=njm1+1
+
+
+x=x1[1:]
+y=y1[1:]
+
+x_2d=np.reshape(x,(njm1,nim1))
+y_2d=np.reshape(y,(njm1,nim1))
+
+x_2d=np.transpose(x_2d)
+y_2d=np.transpose(y_2d)
+
+# compute cell centers
+xp2d= np.zeros((ni,nj))
+yp2d= np.zeros((ni,nj))
+
+for jj in range (0,nj):
+   for ii in range (0,ni):
+
+      im1=max(ii-1,0)
+      jm1=max(jj-1,0)
+
+      i=min(ii,nim1-1)
+      j=min(jj,njm1-1)
+
+
+      xp2d[ii,jj]=0.25*(x_2d[i,j]+x_2d[im1,j]+x_2d[i,jm1]+x_2d[im1,jm1])
+      yp2d[ii,jj]=0.25*(y_2d[i,j]+y_2d[im1,j]+y_2d[i,jm1]+y_2d[im1,jm1])
+
+
+#%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%  read v_1 & transform v_1 to a 3D array (file 1)
+uvw = sio.loadmat('u1_pans_iddes.mat')
+tt=uvw['u1_pans_iddes']
+u3d1= np.reshape(tt,(ni,nj,nk))  #v_1 velocity
+
+uvw = sio.loadmat('v1_pans_iddes.mat')
+tt=uvw['v1_pans_iddes']
+v3d1= np.reshape(tt,(ni,nj,nk))  #v_2 velocity
+
+uvw = sio.loadmat('w1_pans_iddes.mat')
+tt=uvw['w1_pans_iddes']
+w3d1= np.reshape(tt,(ni,nj,nk))  #v_3 velocity
+
+#%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%  read v_1 & transform v_1 to a 3D array (file 2)
+uvw = sio.loadmat('u2_pans_iddes.mat')
+tt=uvw['u2_pans_iddes']
+u3d2= np.reshape(tt,(ni,nj,nk))
+
+uvw = sio.loadmat('v2_pans_iddes.mat')
+tt=uvw['v2_pans_iddes']
+v3d2= np.reshape(tt,(ni,nj,nk))
+
+uvw = sio.loadmat('w2_pans_iddes.mat')
+tt=uvw['w2_pans_iddes']
+w3d2= np.reshape(tt,(ni,nj,nk))
+
+# merge 2 files. This means than new nk = 2*nk
+u3d=np.concatenate((u3d1, u3d2), axis=2)
+v3d=np.concatenate((v3d1, v3d2), axis=2)
+w3d=np.concatenate((w3d1, w3d2), axis=2)
+
+idum,idum,nk=u3d.shape
+
+#u3d = u3d1
+#v3d = v3d1
+#w3d = w3d1
+
+
+# x coordinate direction = index 0, first index
+# y coordinate direction = index 1, second index
+# z coordinate direction = index 2, third index
+
+
+
+ni=len(u3d)
+
+
+#vmean=np.mean(v3d, axis=(2))
+#wmean=np.mean(w3d, axis=(2))
+
+dudx= np.zeros((ni,nj,nk))
+dudy= np.zeros((ni,nj,nk))
+
+d2udx2= np.zeros((ni,nj,nk))
+d2udy2= np.zeros((ni,nj,nk))
+d2udxy= np.zeros((ni,nj,nk))
+
+dvdx= np.zeros((ni,nj,nk))
+dvdy= np.zeros((ni,nj,nk))
+
+d2vdx2= np.zeros((ni,nj,nk))
+d2vdy2= np.zeros((ni,nj,nk))
+d2vdxy= np.zeros((ni,nj,nk))
+
+dwdx= np.zeros((ni,nj,nk))
+dwdy= np.zeros((ni,nj,nk))
+
+d2wdx2= np.zeros((ni,nj,nk))
+d2wdy2= np.zeros((ni,nj,nk))
+d2wdxy= np.zeros((ni,nj,nk))
+
+dphidx= np.zeros((ni,nj))
+dphidy= np.zeros((ni,nj))
+#dummyx= np.zeros((ni,nj))
+#dummyy= np.zeros((ni,nj))
+
+# compute x, y gradients ar grid plane k
+for k in range (1,nk-1):
+   u2d=u3d[:,:,k]
+   v2d=v3d[:,:,k]
+   w2d=w3d[:,:,k]
+
+# u
+   dphidx,dphidy=dphidx_dy(x_2d,y_2d,u2d)
+   dudx[:,:,k]=dphidx
+   dudy[:,:,k]=dphidy
+
+   dummyx,dummyy=dphidx_dy(x_2d,y_2d,dphidx)
+   d2udx2[:,:,k]=dummyx
+
+   dummyx,dummyy=dphidx_dy(x_2d,y_2d,dphidy)
+   d2udy2[:,:,k]=dummyy
+
+# v
+   dphidx,dphidy=dphidx_dy(x_2d,y_2d,v2d)
+   dvdx[:,:,k]=dphidx
+   dvdy[:,:,k]=dphidy
+
+   dummyx,dummyy=dphidx_dy(x_2d,y_2d,dphidx)
+   d2vdx2[:,:,k]=dummyx
+
+   dummyx,dummyy=dphidx_dy(x_2d,y_2d,dphidy)
+   d2vdy2[:,:,k]=dummyy
+
+# w
+   dphidx,dphidy=dphidx_dy(x_2d,y_2d,w2d)
+   dwdx[:,:,k]=dphidx
+   dwdy[:,:,k]=dphidy
+
+   dummyx,dummyy=dphidx_dy(x_2d,y_2d,dphidx)
+   d2wdx2[:,:,k]=dummyx
+
+   dummyx,dummyy=dphidx_dy(x_2d,y_2d,dphidy)
+   d2wdy2[:,:,k]=dummyy
+
+   print('derivarives computed for plane k',k,'out of',nk-2,'planes')
+ 
+
+# compute z gradient (note the the command gradient cannot be used
+# in x and y direction since the mesh is not Cartesian
+# compute 3D instantaneous gradients
+dudz=np.gradient(u3d,dz,axis=2)
+dvdz=np.gradient(v3d,dz,axis=2)
+dwdz=np.gradient(w3d,dz,axis=2)
+
+d2udxz=np.gradient(dudx,dz,axis=2)
+d2udyz=np.gradient(dudy,dz,axis=2)
+d2udz2=np.gradient(dudz,dz,axis=2)
+
+d2vdxz=np.gradient(dvdx,dz,axis=2)
+d2vdyz=np.gradient(dvdy,dz,axis=2)
+d2vdz2=np.gradient(dvdz,dz,axis=2)
+
+d2wdxz=np.gradient(dwdx,dz,axis=2)
+d2wdyz=np.gradient(dwdy,dz,axis=2)
+d2wdz2=np.gradient(dwdz,dz,axis=2)
+
+# U7
+s11=dudx
+s12=0.5*(dudy+dvdx)
+s13=0.5*(dudz+dwdx)
+s21=s12
+s22=dvdy
+s23=0.5*(dvdz+dwdy)
+s31=s13
+s32=s23
+s33=dwdz
+
+ss=(2*(s11**2+s12**2+s13**2+s21**2+s22**2+s23**2+s31**2+s32**2+s33**2)**0.5)
+
+termu=(d2udx2+d2udy2+d2udz2)**2
+termv=(d2vdx2+d2vdy2+d2vdz2)**2
+termw=(d2wdx2+d2wdy2+d2wdz2)**2
+
+termu_b=d2udx2**2+d2udy2**2+2*d2udxy**2
+termv_b=d2vdx2**2+d2vdy2**2+2*d2vdxy**2
+termw_b=d2wdx2**2+d2wdy2**2+2*d2wdxy**2
+
+ubis=(termu+termv+termw)**0.5
+
+ubis_b=(termu_b+termv_b+termw_b)**0.5
+
+L_vk3d=0.4*ss/ubis
+L_vk3d_b=0.4*ss/ubis_b
+
+L_vk3d_spanz=np.mean(L_vk3d, axis=(2))
+
+# read mean velocities
+# read data file
+vectz = np.loadtxt("vectz_fine.dat")
+ntstep=vectz[0]
+ni=int(vectz[1])
+nj=int(vectz[2])
+nk=int(vectz[3])
+n=len(vectz)
+
+nn=14
+nst=3
+iu=range(nst+1,n,nn)
+iv=range(nst+2,n,nn)
+u=vectz[iu]/ntstep
+v=vectz[iv]/ntstep
+umean_2d=np.reshape(u,(ni,nj))
+vmean_2d=np.reshape(v,(ni,nj))
+
+dudx_mean,dudy_mean=dphidx_dy(x_2d,y_2d,umean_2d)
+
+d2udx2_mean,dummy=dphidx_dy(x_2d,y_2d,dudx_mean)
+
+dummy,d2udy2_mean=dphidx_dy(x_2d,y_2d,dudy_mean)
+
+dvdx_mean,dvdy_mean=dphidx_dy(x_2d,y_2d,vmean_2d)
+
+d2vdx2_mean,dummy=dphidx_dy(x_2d,y_2d,dvdx_mean)
+
+dummy,d2vdy2_mean=dphidx_dy(x_2d,y_2d,dvdy_mean)
+
---
+++ Assignment 2b/pl_vect_hump.py
@@ -0,0 +1,368 @@
+import scipy.io as sio
+import numpy as np
+import matplotlib.pyplot as plt
+import tkinter as tk
+from dphidx_dy import dphidx_dy
+from mpl_toolkits.axes_grid1.inset_locator import inset_axes
+plt.rcParams.update({'font.size': 22})
+plt.interactive(True)
+
+re =9.36e+5
+viscos =1/re
+
+xy_hump_fine = np.loadtxt("xy_hump.dat")
+x=xy_hump_fine[:,0]
+y=xy_hump_fine[:,1]
+
+ni=314
+nj=122
+
+nim1=ni-1
+njm1=nj-1
+
+# read data file
+vectz=np.genfromtxt("vectz_aiaa_paper.dat",comments="%")
+ntstep=vectz[0]
+n=len(vectz)
+
+#            write(48,*)uvec(i,j)
+#            write(48,*)vvec(i,j)
+#            write(48,*)dummy(i,j)
+#            write(48,*)uvec2(i,j)
+#            write(48,*)vvec2(i,j)
+#            write(48,*)wvec2(i,j)
+#            write(48,*)uvvec(i,j)
+#            write(48,*)p2D(i,j)
+#            write(48,*)rk2D(i,j)
+#            write(48,*)vis2D(i,j)  
+#            write(48,*)dissp2D(i,j)
+#            write(48,*)uvturb(i,j)
+
+
+
+nn=12
+nst=0
+iu=range(nst+1,n,nn)
+iv=range(nst+2,n,nn)
+ifk=range(nst+3,n,nn)
+iuu=range(nst+4,n,nn)
+ivv=range(nst+5,n,nn)
+iww=range(nst+6,n,nn)
+iuv=range(nst+7,n,nn)
+ip=range(nst+8,n,nn)
+ik=range(nst+9,n,nn)
+ivis=range(nst+10,n,nn)
+idiss=range(nst+11,n,nn)
+iuv_model=range(nst+12,n,nn)
+
+u=vectz[iu]/ntstep
+v=vectz[iv]/ntstep
+fk=vectz[ifk]/ntstep
+uu=vectz[iuu]/ntstep
+vv=vectz[ivv]/ntstep
+ww=vectz[iww]/ntstep
+uv=vectz[iuv]/ntstep
+p=vectz[ip]/ntstep
+k_model=vectz[ik]/ntstep
+vis=vectz[ivis]/ntstep
+diss=vectz[idiss]/ntstep
+uv_model=vectz[iuv_model]/ntstep
+
+# uu is total inst. velocity squared. Hence the resolved turbulent resolved stresses are obtained as
+uu=uu-u**2
+vv=vv-v**2
+uv=uv-u*v
+
+
+p_2d=np.reshape(p,(ni,nj))
+u_2d=np.reshape(u,(ni,nj))
+v_2d=np.reshape(v,(ni,nj))
+fk_2d=np.reshape(fk,(ni,nj))
+uu_2d=np.reshape(uu,(ni,nj))
+uv_2d=np.reshape(uv,(ni,nj))
+vv_2d=np.reshape(vv,(ni,nj))
+ww_2d=np.reshape(ww,(ni,nj))
+k_model_2d=np.reshape(k_model,(ni,nj))
+vis_2d=np.reshape(vis,(ni,nj)) #this is to total viscosity, i.e. vis_tot=vis+vis_turb
+diss_2d=np.reshape(diss,(ni,nj)) 
+x_2d=np.transpose(np.reshape(x,(nj,ni)))
+y_2d=np.transpose(np.reshape(y,(nj,ni)))
+
+# ---- Test
+uv_model-u*v
+uv_model_2d=np.transpose(np.reshape(uv_model,(nj,ni)))
+
+
+# set fk_2d=1 at upper boundary
+fk_2d[:,nj-1]=fk_2d[:,nj-2]
+
+x065_off=np.genfromtxt("x065_off.dat",comments="%")
+x080_off=np.genfromtxt("x080_off.dat",comments="%")
+x090_off=np.genfromtxt("x090_off.dat",comments="%")
+x100_off=np.genfromtxt("x100_off.dat",comments="%")
+x110_off=np.genfromtxt("x110_off.dat",comments="%")
+x120_off=np.genfromtxt("x120_off.dat",comments="%")
+x130_off=np.genfromtxt("x130_off.dat",comments="%")
+
+# the funtion dphidx_dy wants x and y arrays to be one cell smaller than u2d. Hence I take away the last row and column below
+x_2d_new=np.delete(x_2d,-1,0)
+x_2d_new=np.delete(x_2d_new,-1,1)
+y_2d_new=np.delete(y_2d,-1,0)
+y_2d_new=np.delete(y_2d_new,-1,1)
+# compute the gradient
+dudx,dudy=dphidx_dy(x_2d_new,y_2d_new,u_2d)
+
+# ---- Plot 
+# ---- V.1
+def u065():
+    plt.figure("Figure 1")
+    plt.clf() #clear the figure
+    xx=0.65;
+    i1 = (np.abs(xx-x_2d[:,1])).argmin()  # find index which closest fits xx
+    plt.plot(u_2d[i1,:],y_2d[i1,:]-y_2d[i1,0],'b-')
+    plt.plot(x065_off[:,2],x065_off[:,1]-y_2d[i1,0],'bo')
+    plt.xlabel("$U$")
+    plt.ylabel("$y-y_{wall}$")
+    plt.title("$x=0.65$")
+    plt.axis([0, 1.3,0,0.2])
+    
+#*************************
+# plot vv
+def vv065():
+    plt.figure("Figure vv065")
+    plt.clf() #clear the figure
+    xx=0.65;
+    i1 = (np.abs(xx-x_2d[:,1])).argmin()  # find index which closest fits xx
+    plt.plot(vv_2d[i1,:],y_2d[i1,:]-y_2d[i1,0],'b-')
+    plt.plot(x065_off[:,5],x065_off[:,1]-y_2d[i1,0],'bo')
+    plt.xlabel("$\overline{v'v'}$")
+    plt.ylabel("$y-y_{wall}$")
+    plt.title("$x=0.65$")
+    plt.axis([0, 0.01,0,0.2])
+    
+#*************************
+# plot vv
+def vv080():
+    plt.figure("Figure vv080")
+    plt.clf() #clear the figure
+    xx=0.80;
+    i1 = (np.abs(xx-x_2d[:,1])).argmin()  # find index which closest fits xx
+    plt.plot(vv_2d[i1,:],y_2d[i1,:]-y_2d[i1,0],'b-')
+    plt.plot(x080_off[:,5],x080_off[:,1]-y_2d[i1,0],'bo')
+    plt.xlabel("$\overline{v'v'}$")
+    plt.ylabel("$y-y_{wall}$")
+    plt.title("$x=0.80$")
+    plt.axis([0, 0.05,0,0.2])
+
+#*************************
+# plot vv
+def vv090():
+    plt.figure("Figure vv090")
+    plt.clf() #clear the figure
+    xx=0.90;
+    i1 = (np.abs(xx-x_2d[:,1])).argmin()  # find index which closest fits xx
+    plt.plot(vv_2d[i1,:],y_2d[i1,:]-y_2d[i1,0],'b-')
+    plt.plot(x090_off[:,5],x090_off[:,1]-y_2d[i1,0],'bo')
+    plt.xlabel("$\overline{v'v'}$")
+    plt.ylabel("$y-y_{wall}$")
+    plt.title("$x=0.80$")
+    plt.axis([0, 0.05,0,0.2])
+    
+#*************************
+# plot vv
+def vv100():
+    plt.figure("Figure vv100")
+    plt.clf() #clear the figure
+    xx=1.00;
+    i1 = (np.abs(xx-x_2d[:,1])).argmin()  # find index which closest fits xx
+    plt.plot(vv_2d[i1,:],y_2d[i1,:]-y_2d[i1,0],'b-')
+    plt.plot(x100_off[:,5],x100_off[:,1]-y_2d[i1,0],'bo')
+    plt.xlabel("$\overline{v'v'}$")
+    plt.ylabel("$y-y_{wall}$")
+    plt.title("$x=1.00$")
+    plt.axis([0, 0.05,0,0.2])    
+
+#*************************
+# plot vv
+def vv110():
+    plt.figure("Figure vv110")
+    plt.clf() #clear the figure
+    xx=1.10;
+    i1 = (np.abs(xx-x_2d[:,1])).argmin()  # find index which closest fits xx
+    plt.plot(vv_2d[i1,:],y_2d[i1,:]-y_2d[i1,0],'b-')
+    plt.plot(x110_off[:,5],x110_off[:,1]-y_2d[i1,0],'bo')
+    plt.xlabel("$\overline{v'v'}$")
+    plt.ylabel("$y-y_{wall}$")
+    plt.title("$x=1.10$")
+    plt.axis([0, 0.05,0,0.2])    
+
+#*************************
+# plot vv
+def vv120():
+    plt.figure("Figure vv120")
+    plt.clf() #clear the figure
+    xx=1.20;
+    i1 = (np.abs(xx-x_2d[:,1])).argmin()  # find index which closest fits xx
+    plt.plot(vv_2d[i1,:],y_2d[i1,:]-y_2d[i1,0],'b-')
+    plt.plot(x120_off[:,5],x120_off[:,1]-y_2d[i1,0],'bo')
+    plt.xlabel("$\overline{v'v'}$")
+    plt.ylabel("$y-y_{wall}$")
+    plt.title("$x=1.20$")
+    plt.axis([0, 0.05,0,0.2])
+
+#*************************
+# plot vv
+def vv130():
+    plt.figure("Figure vv130")
+    plt.clf() #clear the figure
+    xx=1.30;
+    i1 = (np.abs(xx-x_2d[:,1])).argmin()  # find index which closest fits xx
+    plt.plot(vv_2d[i1,:],y_2d[i1,:]-y_2d[i1,0],'b-')
+    plt.plot(x130_off[:,5],x130_off[:,1]-y_2d[i1,0],'bo')
+    plt.xlabel("$\overline{v'v'}$")
+    plt.ylabel("$y-y_{wall}$")
+    plt.title("$x=1.30$")
+    plt.axis([0, 0.05,0,0.2])    
+    
+#*************************
+# plot uv
+def uv065():
+    plt.figure("Figure uv065")
+    plt.clf() #clear the figure
+    xx=0.65;
+    i1 = (np.abs(xx-x_2d[:,1])).argmin()  # find index which closest fits xx
+    plt.plot(uv_2d[i1,:],y_2d[i1,:]-y_2d[i1,0],'b-')
+    plt.plot(x065_off[:,6],x065_off[:,1]-y_2d[i1,0],'bo')
+    plt.xlabel("$\overline{v'v'}$")
+    plt.ylabel("$y-y_{wall}$")
+    plt.title("$x=1.30$")
+    plt.axis([-0.02, 0.02,0,0.2])
+    
+#*************************
+# plot uv
+def uv130():
+    plt.figure("Figure uv130")
+    plt.clf() #clear the figure
+    xx=1.30;
+    i1 = (np.abs(xx-x_2d[:,1])).argmin()  # find index which closest fits xx
+    plt.plot(uv_2d[i1,:],y_2d[i1,:]-y_2d[i1,0],'b-')
+    plt.plot(x130_off[:,6],x130_off[:,1]-y_2d[i1,0],'bo')
+    plt.xlabel("$\overline{v'v'}$")
+    plt.ylabel("$y-y_{wall}$")
+    plt.title("$x=1.30$")
+    plt.axis([-0.04, 0.02,0,0.2])
+
+# ---- V.2
+
+#*************************
+# plot uv
+def compare_uv065():
+    plt.figure("Figure uv065")
+    plt.clf() #clear the figure
+    xx=0.65;
+    i1 = (np.abs(xx-x_2d[:,1])).argmin()  # find index which closest fits xx
+    plt.plot(uv_2d[i1,:],y_2d[i1,:]-y_2d[i1,0],'b-', label= "Resolved")
+    plt.plot(uv_model_2d[i1,:],y_2d[i1,:]-y_2d[i1,0],'r-', label= "Modelled")
+    plt.xlabel("$\overline{v'v'}$")
+    plt.ylabel("$y-y_{wall}$")
+    plt.title("$x=0.65$")
+    plt.axis([-0.02, 0.02,0,0.2])
+    plt.legend()
+    
+#*************************
+# plot uv
+def compare_uv100():
+    plt.figure("Figure uv100")
+    plt.clf() #clear the figure
+    xx=1.00;
+    i1 = (np.abs(xx-x_2d[:,1])).argmin()  # find index which closest fits xx
+    plt.plot(uv_2d[i1,:],y_2d[i1,:]-y_2d[i1,0],'b-', label= "Resolved")
+    plt.plot(uv_model_2d[i1,:],y_2d[i1,:]-y_2d[i1,0],'r-', label = "Modelled")    
+    plt.xlabel("$\overline{v'v'}$")
+    plt.ylabel("$y-y_{wall}$")
+    plt.title("$x=1.00$")
+    plt.axis([-0.04, 0.02,0,0.2])
+    plt.legend()
+
+# ---- Extras Plots
+################################ contoue plot
+def contour():
+    plt.figure("Figure 4")
+    plt.clf() #clear the figure
+    plt.contourf(x_2d,y_2d,uu_2d, 50)
+    plt.xlabel("$x$")
+    plt.ylabel("$y$")
+    plt.clim(0,0.05)
+    plt.axis([0.6,1.5,0,1])
+    plt.title("contour $\overline{u'u'}$")
+
+################################ vector plot
+def velocity_vector():
+    plt.figure("Figure 5")
+    plt.clf() #clear the figure
+    k=6# plot every forth vector
+    ss=3.2 #vector length
+    plt.quiver(x_2d[::k,::k],y_2d[::k,::k],u_2d[::k,::k],v_2d[::k,::k],width=0.01)
+    plt.xlabel("$x$")
+    plt.ylabel("$y$")
+    plt.axis([0.6,1.5,0,1])
+    plt.title("vector plot")
+    plt.savefig('vect_python.eps')
+
+def close_fig():
+    plt.close()
+    
+root = tk.Tk()
+close_button = tk.Button(root, text='Close plot', command = close_fig)
+close_button.grid(row=0, column=0)
+
+# Overview Plots
+label_overview = tk.Label(text="V.1", background="grey")
+label_overview.grid(row=0, column=1, sticky='nesw')
+
+button_u065 = tk.Button(root, text= 'u065', command = u065)
+button_u065.grid(row=1, column=1, sticky='nesw')
+
+button_vv065 = tk.Button(root, text= 'vv065', command = vv065)
+button_vv065.grid(row=2, column=1, sticky='nesw')
+
+button_vv080 = tk.Button(root, text= 'vv080', command = vv080)
+button_vv080.grid(row=3, column=1, sticky='nesw')
+
+button_vv090 = tk.Button(root, text= 'vv090', command = vv090)
+button_vv090.grid(row=4, column=1, sticky='nesw')
+
+button_vv100 = tk.Button(root, text= 'vv100', command = vv100)
+button_vv100.grid(row=5, column=1, sticky='nesw')
+
+button_vv110 = tk.Button(root, text= 'vv110', command = vv110)
+button_vv110.grid(row=6, column=1, sticky='nesw')
+
+button_vv120 = tk.Button(root, text= 'vv120', command = vv120)
+button_vv120.grid(row=7, column=1, sticky='nesw')
+
+button_vv130 = tk.Button(root, text= 'vv130', command = vv130)
+button_vv130.grid(row=8, column=1, sticky='nesw')
+
+button_uv065 = tk.Button(root, text= 'uv065', command = uv065)
+button_uv065.grid(row=9, column=1, sticky='nesw')
+
+button_uv130 = tk.Button(root, text= 'uv130', command = uv130)
+button_uv130.grid(row=10, column=1, sticky='nesw')
+
+button_compare_uv065 = tk.Button(root, text= 'Comparison uv065', command = compare_uv065)
+button_compare_uv065.grid(row=11, column=1, sticky='nesw')
+
+button_uv100 = tk.Button(root, text= 'Comparison uv100', command = compare_uv100)
+button_uv100.grid(row=12, column=1, sticky='nesw')
+
+# Extra Plots
+button_contour = tk.Button(root, text= 'Contour Plot', command = contour)
+button_contour.grid(row=1, column=0, sticky='nesw')
+
+button_velocity = tk.Button(root, text= 'Velocity Vector Field', command = velocity_vector)
+button_velocity.grid(row=2, column=0, sticky='nesw')
+
+
+
+root.mainloop()
